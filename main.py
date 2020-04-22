7abb051adf4109212183c74069c51cd61ba43ba2
Abulafia <44573666+abul4fia@users.noreply.github.com>
2020-04-22 10:58:50 +0200

    Reimplementación usando convoluciones fft
    Permite llegar a 40fps con universos de 256x256

--- main.py
+++ main.py
@@ -1,57 +1,83 @@
 import numpy as np
+from numpy.fft import fft2, ifft2
 import pyxel
 
-SIZE = 128
+SIZE = 256
 
-pyxel.init(SIZE, SIZE, fps=10)
+pyxel.init(SIZE, SIZE, fps=40)
 
-cell_matrix = (
-    np.zeros((SIZE, SIZE)),
-    np.zeros((SIZE, SIZE))
-)
-counter = 0
+# Basta una matriz en esta implementación
+cell_matrix = np.zeros((SIZE, SIZE))
 
-cell_matrix[0][4, 1] = 1
-cell_matrix[0][5, 2] = 1
-cell_matrix[0][3, 3] = 1
-cell_matrix[0][4, 3] = 1
-cell_matrix[0][5, 3] = 1
+cell_matrix[4, 1] = 1
+cell_matrix[5, 2] = 1
+cell_matrix[3, 3] = 1
+cell_matrix[4, 3] = 1
+cell_matrix[5, 3] = 1
 
-cell_matrix[0][24, 3] = 1
-cell_matrix[0][24, 4] = 1
-cell_matrix[0][24, 5] = 1
+cell_matrix[24, 3] = 1
+cell_matrix[24, 4] = 1
+cell_matrix[24, 5] = 1
 
+def fft_convolve2d(matriz,kernel):
+    """
+    Hace convolución usando fft (no entiendo cómo, pero entiendo qué)
 
-def check_cell(matrix, x: int, y: int) -> bool:
+    Convoluciona con el "kernel" dado toda la matriz
 
-    current_value = matrix[x, y]
-    count = matrix[(x-1) % SIZE,    (y-1) % SIZE] + \
-        matrix[(x) % SIZE,      (y - 1) % SIZE] + \
-        matrix[(x + 1) % SIZE,  (y - 1) % SIZE] + \
-        matrix[(x - 1) % SIZE,  (y) % SIZE] + \
-        matrix[(x + 1) % SIZE,  (y) % SIZE] + \
-        matrix[(x - 1) % SIZE,  (y + 1) % SIZE] + \
-        matrix[(x) % SIZE,      (y + 1) % SIZE] + \
-        matrix[(x+1) % SIZE,    (y+1) % SIZE]
-    if not current_value:
-        return count == 3
-    else:
-        return 1 < count < 4
+    El kernel es otra matriz que se va deslizando "centrándola" en cada elemento
+    de la matriz original (haciendo warping). Entonces se multiplican los valores
+    de ambas matrices, lo que genera otra matriz, se suman todos los elementos
+    de la segunda matriz y así se tiene un valor-resultado para la nueva matriz.
+    A medida que el kernel se va "deslizando", se van generando todos los 
+    valors-resultado de la nueva matriz
+
+    Si se usa un kernel que sea todo ceros, excepto un trocito en el centro así:
+
+       1 1 1
+       1 0 1
+       1 1 1
+
+    El resultado será otra matriz en la que cada celda contiene ahora el número
+    de vecinos que tenía la celda original.
+    """
+    fr = fft2(matriz)
+    fr2 = fft2(np.flipud(np.fliplr(kernel)))
+    m,n = fr.shape
+    cc = np.real(ifft2(fr*fr2))
+    cc = np.roll(cc, - int(m / 2) + 1, axis=0)
+    cc = np.roll(cc, - int(n / 2) + 1, axis=1)
+    return cc
+
+def fft_conway(state, k=None):
+    """
+    Recibe como parámetro la matriz-universo y el kernel a usar
+    (diferentes kernels podrían dar diferentes autómatas celulares)
+    """
+    # Si no recibimos kernel, usamos el apropiado para Life, todo ceros
+    # excepto una pequeña región de 3x3 en el centro que tiene unos alrededor de un cero
+    if k == None:
+        m, n = state.shape
+        k = np.zeros((m, n))
+        k[m//2-1 : m//2+2, n//2-1 : n//2+2] = np.array([[1,1,1],[1,0,1],[1,1,1]])
+
+    # Usamos la fft para hacer la convolución
+    b = fft_convolve2d(state,k).round()
+    # Ahora b contiene en cada celda el número de vecinos vivos
+
+    # Usamos esa información para crear la matriz c con la siguiente generación
+    c = np.zeros(b.shape)
+    c[np.where((b == 2) & (state == 1))] = 1
+    c[np.where((b == 3) & (state == 1))] = 1
+    c[np.where((b == 3) & (state == 0))] = 1
+
+    return c
 
 
 def update():
     global cell_matrix
-    global counter
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
-
-    current_matrix = cell_matrix[counter % 2]
-    counter += 1
-    next_matrix = cell_matrix[counter % 2]
-
-    for y in range(0, SIZE):
-        for x in range(0, SIZE):
-            next_matrix[x, y] = check_cell(current_matrix, x, y)
+    upd()
+    cell_matrix = fft_conway(cell_matrix)
 
 
 def upd():
@@ -61,12 +87,9 @@ def upd():
 
 def draw():
     pyxel.cls(0)
-    global cell_matrix
-    global counter
-    current_matrix = cell_matrix[counter % 2]
     for y in range(0, SIZE):
         for x in range(0, SIZE):
-            pyxel.pset(x, y, 1 if current_matrix[x, y] else 0)
-
+            if cell_matrix[x, y]:    # Pequeña optimización. No pintar celdas vacías
+                pyxel.pset(x, y, 1)
 
 pyxel.run(update, draw)
